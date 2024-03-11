7bb59973bc0c968392b17f9dcb0732c21da4fe8b
Vishalkumar1701 <kvishalkumar.17.01.2000@gmail.com>
2024-03-11 22:59:25 +0530

    about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div className='max-w-6xl mx-auto p-3 space-y-4'>
+      <h1 className='text-2xl font-medium text-amber-600 '>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita, quas! Tenetur vero odio eum ad quaerat perferendis, eos vitae qui, aliquid nisi adipisci voluptate exercitationem atque soluta velit officiis deserunt?
+      </p>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad corporis quasi ullam eius quidem ducimus, ipsam sequi qui aperiam nostrum vitae tempore doloremque temporibus in illo id veritatis voluptas doloribus repellat maxime recusandae nulla repudiandae. Minima sapiente pariatur consectetur quia, autem aliquam officiis non sequi nostrum! Sunt numquam quae dolorum.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem nulla velit itaque soluta reiciendis saepe qui, ea harum eligendi dolor dignissimos rerum voluptas quis ducimus natus aspernatur, fugiat ipsum quod veniam. Impedit, laborum. Aut exercitationem ipsam quis rem commodi quia!</p>
+    </div>
+  )
+}
+
+export default About
